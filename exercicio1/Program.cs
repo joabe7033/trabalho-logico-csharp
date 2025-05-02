cbf0ab8a1b944d6824fa510fb989127c9ece32e1
IamEddo <eduardojose72005@gmail.com>
2025-05-01 22:19:32 -0300

    Adiciona Exercícios 1, 2, 3 e 4 com README dos mesmos

---
+++ exercicio1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma senha: ");
+        string senha = Console.ReadLine();
+
+        bool temMaiuscula = false;
+        bool temNumero = false;
+        bool temEspecial = false;
+
+        foreach (char c in senha)
+        {
+            if (char.IsUpper(c))
+                temMaiuscula = true;
+            else if (char.IsDigit(c))
+                temNumero = true;
+            else if (!char.IsLetterOrDigit(c))
+                temEspecial = true;
+        }
+
+        if (senha.Length >= 8 && temMaiuscula && temNumero && temEspecial)
+        {
+            Console.WriteLine("Senha forte!");
+        }
+        else
+        {
+            Console.WriteLine("Senha fraca. A senha precisa ter:");
+            Console.WriteLine("- Pelo menos 8 caracteres");
+            Console.WriteLine("- Pelo menos uma letra maiúscula");
+            Console.WriteLine("- Pelo menos um número");
+            Console.WriteLine("- Pelo menos um caractere especial");
+        }
+    }
+}
